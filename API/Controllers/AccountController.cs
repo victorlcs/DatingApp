f1276dc43dc950c5edaa6bd8ba38deb5a212b9f1
victorlcs <victor.loh95@gmail.com>
2020-10-03 14:56:09 +0800

    push changes

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,48 @@
+﻿using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+	public class AccountController : BaseApiController
+	{
+		private readonly DataContext _context;
+
+		public AccountController(DataContext context)
+		{
+			_context = context;
+		}
+
+		[HttpPost("register")]
+		public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+		{
+			
+			using var hmac = new HMACSHA512();
+
+			var user = new AppUser
+			{
+				UserName = registerDto.Username,
+				PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+				PasswordSalt = hmac.Key
+			};
+
+			_context.Users.Add(user);
+			await _context.SaveChangesAsync();
+
+			return user;
+		}
+
+		private async Task<bool> UserExists(string username)
+		{
+			return await _context.Users.AnyAsync(x => x.UserName == username);
+		}
+	}
+}
--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -9,13 +9,11 @@ using System.Threading.Tasks;
 
 namespace API.Controllers
 {
-	[ApiController]
-	[Route("api/[controller]")]
-	public class UserController : ControllerBase
+	public class UsersController : BaseApiController
 	{
 		private readonly DataContext _context;
 
-		public UserController(DataContext context)
+		public UsersController(DataContext context)
 		{
 			_context = context;
 		}
